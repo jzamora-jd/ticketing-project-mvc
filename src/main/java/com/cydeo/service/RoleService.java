0489472ba21d71210ebdf38cdba11af03a235855
Zamora <jzamora.jd@gmail.com>
2024-05-06 22:46:49 -0500

    added crud service implementation

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,13 +1,5 @@
 package com.cydeo.service;
+import com.cydeo.dto.UserDTO;
 
-import com.cydeo.dto.RoleDTO;
-
-import java.util.List;
-
-public interface RoleService {
-    RoleDTO save(RoleDTO roleDTO);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-    void delete(RoleDTO roleDTO);
-    void deleteById(Long id);
+public interface RoleService extends CrudService<UserDTO,Long> {
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,11 +1,6 @@
 package com.cydeo.service;
 import com.cydeo.dto.UserDTO;
-import java.util.List;
 
-public interface UserService {
-    UserDTO save(UserDTO userDTO);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void delete(UserDTO userDTO);
-    void deleteById(String username);
+public interface UserService extends CrudService<UserDTO,String>{
+
 }
