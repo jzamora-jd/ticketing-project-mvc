6b9e0c86c6c680d3e9d75ce6fe0cbbb6cfb06a36
Zamora <jzamora.jd@gmail.com>
2024-05-31 16:38:20 -0500

    completed delete controller

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -62,4 +62,12 @@ public class UserController {
         return "redirect:/user/create";
     }
 
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
 }
