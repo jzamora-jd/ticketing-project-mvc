3c26d1070f05b56d28140cc5213be5d4199a46de
Zamora <jzamora.jd@gmail.com>
2024-06-03 16:20:38 -0500

    finished status update complete

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -37,4 +37,11 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> im
     public ProjectDTO findById(String id) {
         return super.findById(id);
     }
+
+
+    @Override
+    public void complete(ProjectDTO project) {
+        project.setProjectStatus(Status.COMPLETE);
+        super.save(project.getProjectCode(),project);
+    }
 }
