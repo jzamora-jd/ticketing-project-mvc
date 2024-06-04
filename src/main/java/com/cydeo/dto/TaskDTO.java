ea36fafc8653ddecd2ff47c1f952b73babaca004
Zamora <jzamora.jd@gmail.com>
2024-06-03 19:12:02 -0500

    added task DTO

---
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -0,0 +1,24 @@
+package com.cydeo.dto;
+
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TaskDTO {
+
+    private Long id;
+    private ProjectDTO projectDTO;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    private Status taskStatus;
+    private LocalDate assignedDate;
+
+}
