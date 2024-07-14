305c7f434112bb2df588cc1ae0190743c283794f
Zamora <jzamora.jd@gmail.com>
2024-07-14 12:26:39 -0500

    revised service to user DB

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -2,8 +2,6 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.enums.Status;
-import com.cydeo.service.ProjectService;
-import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,51 +13,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/task")
 public class TaskController {
-
-    private final TaskService taskService;
-    private final ProjectService projectService;
-    private final UserService userService;
-
-    public TaskController(TaskService taskService, ProjectService projectService, UserService userService) {
-        this.taskService = taskService;
-        this.projectService = projectService;
-        this.userService = userService;
-    }
-
-    @GetMapping("/create")
-    public String createTask(Model model) {
-
-        model.addAttribute("task", new TaskDTO());
-        model.addAttribute("projects", projectService.findAll());
-        model.addAttribute("employees", userService.findEmployees());
-        model.addAttribute("tasks", taskService.findAll());
-
-        return "task/create";
-    }
-
-    @PostMapping("/create")
-    public String insertTask(TaskDTO task) {
-        taskService.save(task);
-        return "redirect:/task/create";
-    }
-
-    @GetMapping("/delete/{taskId}")
-    public String deleteTask(@PathVariable("taskId") Long taskId) {
-        taskService.deleteById(taskId);
-        return "redirect:/task/create";
-    }
-
-    @GetMapping("/update/{taskId}")
-    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
-
-        model.addAttribute("task", taskService.findById(taskId));
-        model.addAttribute("projects", projectService.findAll());
-        model.addAttribute("employees", userService.findEmployees());
-        model.addAttribute("tasks", taskService.findAll());
-
-        return "task/update";
-
-    }
+//
+//    private final TaskService taskService;
+//    private final ProjectService projectService;
+//    private final UserService userService;
+//
+//    public TaskController(TaskService taskService, ProjectService projectService, UserService userService) {
+//        this.taskService = taskService;
+//        this.projectService = projectService;
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/create")
+//    public String createTask(Model model) {
+//
+//        model.addAttribute("task", new TaskDTO());
+//        model.addAttribute("projects", projectService.findAll());
+//        model.addAttribute("employees", userService.findEmployees());
+//        model.addAttribute("tasks", taskService.findAll());
+//
+//        return "task/create";
+//    }
+//
+//    @PostMapping("/create")
+//    public String insertTask(TaskDTO task) {
+//        taskService.save(task);
+//        return "redirect:/task/create";
+//    }
+//
+//    @GetMapping("/delete/{taskId}")
+//    public String deleteTask(@PathVariable("taskId") Long taskId) {
+//        taskService.deleteById(taskId);
+//        return "redirect:/task/create";
+//    }
+//
+//    @GetMapping("/update/{taskId}")
+//    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
+//
+//        model.addAttribute("task", taskService.findById(taskId));
+//        model.addAttribute("projects", projectService.findAll());
+//        model.addAttribute("employees", userService.findEmployees());
+//        model.addAttribute("tasks", taskService.findAll());
+//
+//        return "task/update";
+//
+//    }
 
 //    @PostMapping("/update/{taskId}")
 //    public String updateTask(@PathVariable("taskId") Long taskId,TaskDTO task) {
@@ -69,41 +67,41 @@ public class TaskController {
 //    }
 //above code is similar to code below: Spring sets id automatically since its sees field id in task DTO
 
-    @PostMapping("/update/{id}")
-    public String updateTask(TaskDTO task) {
-        taskService.update(task);
-        return "redirect:/task/create";
-    }
-
-    @GetMapping("/employee/pending-tasks")
-    public String employeePendingTasks(Model model) {
-        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-        return "task/pending-tasks";
-    }
-
-    @GetMapping("/employee/edit/{id}")
-    public String employeeEditTask(@PathVariable("id") Long id, Model model) {
-
-        model.addAttribute("task", taskService.findById(id));
-        model.addAttribute("employees", userService.findEmployees());
-        model.addAttribute("projects", projectService.findAllNonCompletedProjects());
-        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-        model.addAttribute("statuses", Status.values());
-
-        return "task/status-update";
-
-    }
-
-    @PostMapping("/employee/update/{id}")
-    public String employeeUpdateTask(TaskDTO taskDTO) {
-        taskService.updateStatus(taskDTO);
-        return "redirect:/task/employee/pending-tasks";
-    }
-
-    @GetMapping("/employee/archive")
-    public String employeeArchivedTasks(Model model) {
-        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
-        return "task/archive";
-    }
+//    @PostMapping("/update/{id}")
+//    public String updateTask(TaskDTO task) {
+//        taskService.update(task);
+//        return "redirect:/task/create";
+//    }
+//
+//    @GetMapping("/employee/pending-tasks")
+//    public String employeePendingTasks(Model model) {
+//        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+//        return "task/pending-tasks";
+//    }
+//
+//    @GetMapping("/employee/edit/{id}")
+//    public String employeeEditTask(@PathVariable("id") Long id, Model model) {
+//
+//        model.addAttribute("task", taskService.findById(id));
+//        model.addAttribute("employees", userService.findEmployees());
+//        model.addAttribute("projects", projectService.findAllNonCompletedProjects());
+//        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+//        model.addAttribute("statuses", Status.values());
+//
+//        return "task/status-update";
+//
+//    }
+//
+//    @PostMapping("/employee/update/{id}")
+//    public String employeeUpdateTask(TaskDTO taskDTO) {
+//        taskService.updateStatus(taskDTO);
+//        return "redirect:/task/employee/pending-tasks";
+//    }
+//
+//    @GetMapping("/employee/archive")
+//    public String employeeArchivedTasks(Model model) {
+//        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
+//        return "task/archive";
+//    }
 
 }
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
-import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
-import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
-    RoleService roleService;
-    UserService userService;
-
-    public UserController(RoleService roleService, UserService userService) {
-        this.roleService = roleService;
-        this.userService = userService;
-    }
-
+//    RoleService roleService;
+//    UserService userService;
+//
+//    public UserController(RoleService roleService, UserService userService) {
+//        this.roleService = roleService;
+//        this.userService = userService;
+//    }
+//
     @GetMapping("/create")
     public String createUser(Model model) {
 
@@ -30,44 +28,44 @@ public class UserController {
         return "/user/create";
     }
 
-    @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user, Model model) {
-
-        model.addAttribute("user", new UserDTO());
-        model.addAttribute("roles", roleService.findAll());
-
-        userService.save(user);
-
-        model.addAttribute("users", userService.findAll());
-
-        return "redirect:/user/create";
-
-    }
-
-    @GetMapping("/update/{username}")
-    public String editUser(@PathVariable("username") String username,Model model) {
-
-        model.addAttribute("user", userService.findById(username));
-        model.addAttribute("roles", userService.findById(username).getRole());
-        model.addAttribute("users", userService.findAll());
-
-        return "/user/update";
-    }
-
-    @PostMapping("/update")
-    public String updateUser(UserDTO user){
-
-        userService.update(user);
-
-        return "redirect:/user/create";
-    }
-
-    @GetMapping("/delete/{username}")
-    public String deleteUser(@PathVariable("username") String username){
-
-        userService.deleteById(username);
-
-        return "redirect:/user/create";
-    }
+//    @PostMapping("/create")
+//    public String insertUser(@ModelAttribute("user") UserDTO user, Model model) {
+//
+//        model.addAttribute("user", new UserDTO());
+//        model.addAttribute("roles", roleService.findAll());
+//
+//        userService.save(user);
+//
+//        model.addAttribute("users", userService.findAll());
+//
+//        return "redirect:/user/create";
+//
+//    }
+//
+//    @GetMapping("/update/{username}")
+//    public String editUser(@PathVariable("username") String username,Model model) {
+//
+//        model.addAttribute("user", userService.findById(username));
+//        model.addAttribute("roles", userService.findById(username).getRole());
+//        model.addAttribute("users", userService.findAll());
+//
+//        return "/user/update";
+//    }
+//
+//    @PostMapping("/update")
+//    public String updateUser(UserDTO user){
+//
+//        userService.update(user);
+//
+//        return "redirect:/user/create";
+//    }
+//
+//    @GetMapping("/delete/{username}")
+//    public String deleteUser(@PathVariable("username") String username){
+//
+//        userService.deleteById(username);
+//
+//        return "redirect:/user/create";
+//    }
 
 }
