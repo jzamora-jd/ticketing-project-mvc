749994245bc5e5c315856111aaf4b0d3376691dc
Zamora <jzamora.jd@gmail.com>
2024-07-14 16:57:54 -0500

    completed role dto converter

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -3,18 +3,22 @@ package com.cydeo.service.impl;
 import com.cydeo.Repository.RoleRepository;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository,RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
@@ -22,12 +26,12 @@ public class RoleServiceImpl implements RoleService {
         //controller requesting all usrs
         //first go to db and bring all the users from there
 
-        List<Role> roleList =  roleRepository.findAll();
+//        List<Role> roleList =  roleRepository.findAll();
+//
+//        //convert entity list to roleDto list - get roles from db and convert each role to roleDto using streams
+//        List<RoleDTO> list2 = roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
 
-        //convert entity list to roleDto list
-
-
-        return null;
+        return  roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());//one line code
     }
 
     @Override
